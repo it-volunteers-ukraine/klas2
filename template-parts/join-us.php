7f6eeb9f680426a072030033047753ac08f60a30
Diana Forostiana <dianaforost@gmail.com>
2023-11-09 18:57:26 +0100

    feat: join-us section & styles (little about page)

---
+++ template-parts/join-us.php
@@ -0,0 +1,9 @@
+<section class="join-us">
+  <div class="container join-us__container">
+    <div class="join-us__content">
+      <h3 class="join-us__title">Команда ГО “Клас” реалізує соціальні проєкти, не заробляючи на цьому</h3>
+    <p class="join-us__text">Нам дуже важливо відчувати підтримку однодумців. Долучайтеся до спільноти доброчинців та творімо добро разом із нами</p>
+    </div>
+    <a href="/contacts" class="join-us__button" aria-label="Долучитися">Долучитися</a>
+  </div>
+</section>
