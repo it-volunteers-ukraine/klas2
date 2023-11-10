e8c223441c74602c27d8bc1c43aa78544fbbfdbe
Smilichenko <112238837+OlgaSmile@users.noreply.github.com>
2023-11-10 08:08:28 +0200

    added css for read-more button

--- template-parts/form.php
+++ template-parts/form.php
@@ -1,6 +1,7 @@
 <section class="helpform__section">
     <div class="container">
-        <h1 class="helpform__title"><?php the_field('form_title'); ?></h1>
+        <h1 class="helpform__title"><?php the_field('form-title'); ?></h1>
+        <h2 class="helpform__title"><?php the_field('form-subtitle'); ?></h2>
         <?php 
             $form = get_field('form');
             if($form) : ?>
