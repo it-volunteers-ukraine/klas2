60fa77074004e6a9aa097069400d86af99ab04b9
Diana Forostiana <dianaforost@gmail.com>
2023-11-14 13:11:24 +0100

    fix: styles for join-us

--- template-parts/join-us.php
+++ template-parts/join-us.php
@@ -12,7 +12,7 @@
     </div>
     <?php $button = get_field("join_us_button"); ?>
       <?php if ($button): ?>
-        <a href="<?php the_field("url") ?>" class="join-us__button"><?php echo $button; ?></a>
+        <a href="<?php the_field("url") ?>" class="join-us__button button"><?php echo $button; ?></a>
       <?php endif; ?>
   </div>
 </section>
