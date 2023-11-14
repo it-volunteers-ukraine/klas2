34a8f89029d30ad1e06fd0fd3ccac3a5acd6b933
litvinenkoiryna <litvinenkoiryna@gmail.com>
2023-11-14 22:01:59 +0200

    ready

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -5,9 +5,16 @@ menuButton.addEventListener('click', function(){
     this.classList.toggle('active');
     menu.classList.toggle('open');
     document.body.classList.toggle("modal-open");
+    menu.addEventListener("click", closeByBgdClick);
     
     
 })
-
+function closeByBgdClick(e) {
+    if (e.target === menu) {
+        menu.classList.remove('open');
+        menuButton.classList.remove('active');
+    }
+  }
+  
 
 console.log("header");
