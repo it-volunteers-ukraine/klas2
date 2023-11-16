1b7ed369e84a8ec8d03011cd4878e994567c05a6
Tetiana Fedorenko <1031213@gmail.com>
2023-11-16 15:49:56 +0200

    feat: add last blocks/ partly responsive

--- front-page.php
+++ front-page.php
@@ -29,17 +29,26 @@
                 </div>
                 <div class="banner__cards">
                     <div class="banner__cards-card">
-                        <img src="<?php echo get_template_directory_uri() ?>/src/images/front-page/40.svg" alt>
+                        <div class="banner__cards-card-img">
+                            <img src="<?php echo get_template_directory_uri() ?>/src/images/front-page/40.svg" alt>
+                        </div>
+
                         <p>виконаних проєктів</p>
 
                     </div>
                     <div class="banner__cards-card">
+                        <div class="banner__cards-card-img">
                         <img src="<?php echo get_template_directory_uri() ?>/src/images/front-page/8.svg" alt>
+                        </div>
+                       
                         <p>проєктів в роботі </p>
 
                     </div>
                     <div class="banner__cards-card">
-                        <img src="<?php echo get_template_directory_uri() ?>/src/images/front-page/Vector.svg" alt>
+                    <div class="banner__cards-card-img">
+                    <img src="<?php echo get_template_directory_uri() ?>/src/images/front-page/Vector.svg" alt>
+                        </div>
+                       
                         <p>бажання допомагати </p>
 
                     </div>
@@ -60,42 +69,45 @@
                 <h2 class="title">
                     Хто ми
                 </h2>
-                <div class="about__content content">
+                <div class="about__content content content--right">
                     <div class="content__desc">
                         <div class="content__desc-text">
                             <div class="content__fon">
                                 <img src="<?php echo get_template_directory_uri() ?>/src/images/front-page/about.jpg" alt>
                             </div>
-                            <p> Вже понад 25 років наша організація
-                                допомагає
-                                дітям
-                                та молоді, які опинились у складних
-                                життєвих
-                                обставинах, надає психологічну
-                                підтримку,
-                                соціальний
-                                супровід та кар’єрне консультування.
-                            </p>
-                            <p>
-                                Ми реалізували багато проектів, провели
-                                безліч
-                                корисних, освітніх тренингів та відкрили
-                                декілька
-                                важливих для суспільства установ.
-                            </p>
+                            <div>
+                                <p> Вже понад 25 років наша організація
+                                    допомагає
+                                    дітям
+                                    та молоді, які опинились у складних
+                                    життєвих
+                                    обставинах, надає психологічну
+                                    підтримку,
+                                    соціальний
+                                    супровід та кар’єрне консультування.
+                                </p>
+                                <p>
+                                    Ми реалізували багато проектів, провели
+                                    безліч
+                                    корисних, освітніх тренингів та відкрили
+                                    декілька
+                                    важливих для суспільства установ.
+                                </p>
 
-                            <p> Зараз наша команда активно надає
-                                гуманітарну та
-                                психосоціальну допомогу людям, що
-                                постраждали
-                                від
-                                воєнного конфлікту в Україні, ми
-                                створюємо
-                                безпечні
-                                умови для евакуйованих дітей, молоді та
-                                сімей з
-                                дітьми.
-                            </p>
+                                <p> Зараз наша команда активно надає
+                                    гуманітарну та
+                                    психосоціальну допомогу людям, що
+                                    постраждали
+                                    від
+                                    воєнного конфлікту в Україні, ми
+                                    створюємо
+                                    безпечні
+                                    умови для евакуйованих дітей, молоді та
+                                    сімей з
+                                    дітьми.
+                                </p>
+                            </div>
+
 
                             <div style="clear:both"></div>
 
@@ -129,8 +141,7 @@
                                 педагогічної,
                                 психологічної та соціальної підтримки</p>
                             <p> Розвиток життєвих та соціальних навичок,
-                                а
-                                також вмінь встановлювати здорові
+                                а також вмінь встановлювати здорові
                                 відносини
                                 і мирно вирішувати конфлікти
                             </p>
@@ -199,15 +210,15 @@
                             <div class="new__desc">
                                 <h3>Ігротека для дітей з інвалідністю та робота з батьками</h3>
                                 <p>
-                                    Сьогодні фахівці 
+                                    Сьогодні фахівці
                                     дитячої точки СПІЛЬНО провели для дітей
-                                     ігротеку на дитячому майданчику «Кмітливі пірати» та пограл
+                                    ігротеку на дитячому майданчику «Кмітливі пірати» та пограл
                                 </p>
                                 <div class="new__footer">
                                     <time datatime='2023-06-24'>24.06.23</time>
                                     <a href="#">Читати більше<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                <path fill-rule="evenodd" clip-rule="evenodd" d="M5.46967 5.46967C5.76256 5.17678 6.23744 5.17678 6.53033 5.46967L18.25 17.1893V6.52C18.25 6.10579 18.5858 5.77 19 5.77C19.4142 5.77 19.75 6.10579 19.75 6.52V19C19.75 19.4142 19.4142 19.75 19 19.75H6.52C6.10579 19.75 5.77 19.4142 5.77 19C5.77 18.5858 6.10579 18.25 6.52 18.25H17.1893L5.46967 6.53033C5.17678 6.23744 5.17678 5.76256 5.46967 5.46967Z" fill="#070707" />
-                            </svg></a>
+                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M5.46967 5.46967C5.76256 5.17678 6.23744 5.17678 6.53033 5.46967L18.25 17.1893V6.52C18.25 6.10579 18.5858 5.77 19 5.77C19.4142 5.77 19.75 6.10579 19.75 6.52V19C19.75 19.4142 19.4142 19.75 19 19.75H6.52C6.10579 19.75 5.77 19.4142 5.77 19C5.77 18.5858 6.10579 18.25 6.52 18.25H17.1893L5.46967 6.53033C5.17678 6.23744 5.17678 5.76256 5.46967 5.46967Z" fill="#070707" />
+                                        </svg></a>
                                 </div>
                             </div>
                         </div>
@@ -221,15 +232,15 @@
                             <div class="new__desc">
                                 <h3>Ігротека для дітей з інвалідністю та робота з батьками</h3>
                                 <p>
-                                    Сьогодні фахівці 
+                                    Сьогодні фахівці
                                     дитячої точки СПІЛЬНО провели для дітей
-                                     ігротеку на дитячому майданчику «Кмітливі пірати» та пограл
+                                    ігротеку на дитячому майданчику «Кмітливі пірати» та пограл
                                 </p>
                                 <div class="new__footer">
                                     <time datatime='2023-06-24'>24.06.23</time>
                                     <a href="#">Читати більше<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                <path fill-rule="evenodd" clip-rule="evenodd" d="M5.46967 5.46967C5.76256 5.17678 6.23744 5.17678 6.53033 5.46967L18.25 17.1893V6.52C18.25 6.10579 18.5858 5.77 19 5.77C19.4142 5.77 19.75 6.10579 19.75 6.52V19C19.75 19.4142 19.4142 19.75 19 19.75H6.52C6.10579 19.75 5.77 19.4142 5.77 19C5.77 18.5858 6.10579 18.25 6.52 18.25H17.1893L5.46967 6.53033C5.17678 6.23744 5.17678 5.76256 5.46967 5.46967Z" fill="#070707" />
-                            </svg></a>
+                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M5.46967 5.46967C5.76256 5.17678 6.23744 5.17678 6.53033 5.46967L18.25 17.1893V6.52C18.25 6.10579 18.5858 5.77 19 5.77C19.4142 5.77 19.75 6.10579 19.75 6.52V19C19.75 19.4142 19.4142 19.75 19 19.75H6.52C6.10579 19.75 5.77 19.4142 5.77 19C5.77 18.5858 6.10579 18.25 6.52 18.25H17.1893L5.46967 6.53033C5.17678 6.23744 5.17678 5.76256 5.46967 5.46967Z" fill="#070707" />
+                                        </svg></a>
                                 </div>
                             </div>
                         </div>
@@ -243,23 +254,161 @@
                             <div class="new__desc">
                                 <h3>Ігротека для дітей з інвалідністю та робота з батьками</h3>
                                 <p>
-                                    Сьогодні фахівці 
+                                    Сьогодні фахівці
                                     дитячої точки СПІЛЬНО провели для дітей
-                                     ігротеку на дитячому майданчику «Кмітливі пірати» та пограл
+                                    ігротеку на дитячому майданчику «Кмітливі пірати» та пограл
                                 </p>
                                 <div class="new__footer">
                                     <time datatime='2023-06-24'>24.06.23</time>
                                     <a href="#">Читати більше<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                <path fill-rule="evenodd" clip-rule="evenodd" d="M5.46967 5.46967C5.76256 5.17678 6.23744 5.17678 6.53033 5.46967L18.25 17.1893V6.52C18.25 6.10579 18.5858 5.77 19 5.77C19.4142 5.77 19.75 6.10579 19.75 6.52V19C19.75 19.4142 19.4142 19.75 19 19.75H6.52C6.10579 19.75 5.77 19.4142 5.77 19C5.77 18.5858 6.10579 18.25 6.52 18.25H17.1893L5.46967 6.53033C5.17678 6.23744 5.17678 5.76256 5.46967 5.46967Z" fill="#070707" />
-                            </svg></a>
+                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M5.46967 5.46967C5.76256 5.17678 6.23744 5.17678 6.53033 5.46967L18.25 17.1893V6.52C18.25 6.10579 18.5858 5.77 19 5.77C19.4142 5.77 19.75 6.10579 19.75 6.52V19C19.75 19.4142 19.4142 19.75 19 19.75H6.52C6.10579 19.75 5.77 19.4142 5.77 19C5.77 18.5858 6.10579 18.25 6.52 18.25H17.1893L5.46967 6.53033C5.17678 6.23744 5.17678 5.76256 5.46967 5.46967Z" fill="#070707" />
+                                        </svg></a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </article>
+                    <article class="new new--column front-page__news-new">
+                        <div class="new__body">
+                            <div class="new__back">
+                                <img src="<?php echo get_template_directory_uri() ?>/src/images/front-page/new1.jpg" alt="">
+                            </div>
+                            <div class="new__desc">
+                                <h3>Ігротека для дітей з інвалідністю та робота з батьками</h3>
+                                <p>
+                                    Сьогодні фахівці
+                                    дитячої точки СПІЛЬНО провели для дітей
+                                    ігротеку на дитячому майданчику «Кмітливі пірати» та пограл
+                                </p>
+                                <div class="new__footer">
+                                    <time datatime='2023-06-24'>24.06.23</time>
+                                    <a href="#">Читати більше<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M5.46967 5.46967C5.76256 5.17678 6.23744 5.17678 6.53033 5.46967L18.25 17.1893V6.52C18.25 6.10579 18.5858 5.77 19 5.77C19.4142 5.77 19.75 6.10579 19.75 6.52V19C19.75 19.4142 19.4142 19.75 19 19.75H6.52C6.10579 19.75 5.77 19.4142 5.77 19C5.77 18.5858 6.10579 18.25 6.52 18.25H17.1893L5.46967 6.53033C5.17678 6.23744 5.17678 5.76256 5.46967 5.46967Z" fill="#070707" />
+                                        </svg></a>
                                 </div>
                             </div>
                         </div>
 
                     </article>
                 </div>
+                <a class="primary_button button front-page__news-btn" href=" ">
+                    Усі новини
+                </a>
             </div>
 
+        </div>
+    </section>
+    <section class="front-page__slider">
+        <div class="container">
+            <h2 class="title">
+                Наші партнери
+            </h2>
+        </div>
+        <div class="front-page__slider-wrap slider">
+            <div class="slider__inner">
+                <div class="slider__item">
+                    <img src="<?php echo get_template_directory_uri() ?>/src/images/front-page/new1.jpg" alt="">
+                </div>
+                <div class="slider__item">
+                    <img src="<?php echo get_template_directory_uri() ?>/src/images/front-page/new1.jpg" alt="">
+                </div>
+                <div class="slider__item">
+                    <img src="<?php echo get_template_directory_uri() ?>/src/images/front-page/new1.jpg" alt="">
+                </div>
+                <div class="slider__item">
+                    <img src="<?php echo get_template_directory_uri() ?>/src/images/front-page/new1.jpg" alt="">
+                </div>
+                <div class="slider__item">
+                    <img src="<?php echo get_template_directory_uri() ?>/src/images/front-page/new1.jpg" alt="">
+                </div>
+                <div class="slider__item">
+                    <img src="<?php echo get_template_directory_uri() ?>/src/images/front-page/new1.jpg" alt="">
+                </div>
+                <div class="slider__item">
+                    <img src="<?php echo get_template_directory_uri() ?>/src/images/front-page/new1.jpg" alt="">
+                </div>
+                <div class="slider__item">
+                    <img src="<?php echo get_template_directory_uri() ?>/src/images/front-page/new1.jpg" alt="">
+                </div>
+                <div class="slider__item">
+                    <img src="<?php echo get_template_directory_uri() ?>/src/images/front-page/new1.jpg" alt="">
+                </div>
+                <div class="slider__item">
+                    <img src="<?php echo get_template_directory_uri() ?>/src/images/front-page/new1.jpg" alt="">
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <section class="collaboration">
+        <div class="container">
+            <div class="collaboration__wrap">
+                <div class="collaboration__title">
+                    <h4>
+                        Про важливість партнерства
+                    </h4>
+                </div>
+
+                <div class="collaboration__content content content--left">
+                    <div class="content__desc">
+                        <div class="content__desc-text">
+                            <div class="collaboration__content-fon content__fon">
+                                <img src="http://klas2/wp-content/themes/klas2/src/images/front-page/about.jpg" alt="">
+                            </div>
+
+                            <p> Вже понад 25 років наша організація
+                                допомагає
+                                дітям
+                                та молоді, які опинились у складних
+                                життєвих
+                                обставинах, надає психологічну
+                                підтримку,
+                                соціальний
+                                супровід та кар’єрне консультування.
+                            </p>
+                            <p>
+                                Ми реалізували багато проектів, провели
+                                безліч
+                                корисних, освітніх тренингів та відкрили
+                                декілька
+                                важливих для суспільства установ.
+                            </p>
+
+                            <p> Зараз наша команда активно надає
+                                гуманітарну та
+                                психосоціальну допомогу людям, що
+                                постраждали
+                                від
+                                воєнного конфлікту в Україні, ми
+                                створюємо
+                                безпечні
+                                умови для евакуйованих дітей, молоді та
+                                сімей з
+                                дітьми.
+                            </p>
+
+
+
+                            <div style="clear:both"></div>
+
+
+                        </div>
+
+
+
+
+                    </div>
+
+
+
+                </div>
+                <div class="collaboration__btn">
+                    <a class="primary_button button" href=" ">
+                        Стати партнером
+                    </a>
+                </div>
+
+            </div>
         </div>
     </section>
 </main>
