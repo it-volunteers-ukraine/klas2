6c565a70006d664d6bacf5db1fc715410a848c36
Diana Forostiana <dianaforost@gmail.com>
2023-11-20 19:27:59 +0100

    fix: we are section

--- templates/about.php
+++ templates/about.php
@@ -42,41 +42,31 @@ get_header();
     </div>
   </div>
 </section>
-<section class="we-are">
-<div class="container">
-<?php $weAreTitle = get_field("we_are_title"); ?>
-<?php if ($weAreTitle): ?>
-  <h2 class="we-are__title"><?php echo esc_html($weAreTitle); ?></h2>
-  <ul class="we-are__list">
-    <li class="we-are__list__item">
-      <h3 class="we-are__list__item__title"><?php the_field("we_are_first_title"); ?></h3>
-      <p class="we-are__list__item__text"><?php the_field("we_are_first_text"); ?></p>
-    </li>
-    <li class="we-are__list__item">
-      <h3 class="we-are__list__item__title"><?php the_field("we_are_second_title"); ?></h3>
-      <p class="we-are__list__item__text"><?php the_field("we_are_second_text"); ?></p>
-    </li>
-    <li class="we-are__list__item">
-      <h3 class="we-are__list__item__title"><?php the_field("we_are_third_title"); ?></h3>
-      <p class="we-are__list__item__text"><?php the_field("we_are_third_text"); ?></p>
-    </li>
 
-    <li class="we-are__list__item">
-      <h3 class="we-are__list__item__title"><?php the_field("we_are_fourth_title"); ?></h3>
-      <p class="we-are__list__item__text"><?php the_field("we_are_fourth_text"); ?></p>
-    </li>
-    <li class="we-are__list__item">
-      <h3 class="we-are__list__item__title"><?php the_field("we_are_fifth_title"); ?></h3>
-      <p class="we-are__list__item__text"><?php the_field("we_are_fifth_text"); ?></p>
-    </li>
-    <li class="we-are__list__item">
-      <h3 class="we-are__list__item__title"><?php the_field("we_are_sixth_title"); ?></h3>
-      <p class="we-are__list__item__text"><?php the_field("we_are_sixth_text"); ?></p>
-    </li>
-  </ul>
-      <?php endif; ?>
-</div>
+
+<section class="we-are">
+  <div class="container">
+  <?php if(have_rows('we_are')): ?>
+    <?php $weAreTitle = get_field("we_are_title"); ?>
+      <?php if ($weAreTitle): ?>
+        <h2 class="we-are__title"><?php echo esc_html($weAreTitle); ?></h2>
+        <?php endif; ?>
+          <ul class="we-are__list">
+            <?php while(have_rows('we_are')) : the_row(); 
+                          $title = get_sub_field('we_are_title');
+                          $text = get_sub_field('we_are_text');
+                        ?>
+                <li class="we-are__list__item">
+                  <h3 class="we-are__list__item__title"><?php echo $title ?></h3>
+                  <p class="we-are__list__item__text"><?php echo $text ?></p>
+              </li>
+            <?php endwhile; ?>
+        </ul>
+    <?php endif; ?>
+  </div>
 </section>
+
+
 <section class="our-team">
   <div class="container our-team__container">
     <?php if(have_rows('our_team_slider')): ?>
@@ -93,20 +83,17 @@ get_header();
         <div class="swiper-container">
           <div class="our-team__slider swiper">
           <ul class="our-team__list swiper-wrapper">
-                <?php while(have_rows('our_team_slider')) : the_row(); 
+              <?php while(have_rows('our_team_slider')) : the_row(); 
                   $image = get_sub_field('our_team_slider_image');
                   $person = get_sub_field('our_team_slider_person');
                   $position = get_sub_field('our_team_slider_position');
-                ?>
+              ?>
                 <li class="our-team__list__item swiper-slide">
-
-                <img class="our-team__list__item__img" src='<?php echo $image['url'] ?>' />
-
-                <div class="our-team__list__item__text-container">
-                <h3 class="our-team__list__item__person"><?php echo $person ?></h3>
-                  <p class="our-team__list__item__position"><?php echo $position ?></p>
-                </div>
-                
+                    <img class="our-team__list__item__img" src='<?php echo $image['url'] ?>' />
+                    <div class="our-team__list__item__text-container">
+                      <h3 class="our-team__list__item__person"><?php echo $person ?></h3>
+                      <p class="our-team__list__item__position"><?php echo $position ?></p>
+                    </div>
                 </li>
                 <?php endwhile; ?>
               </ul>
@@ -117,6 +104,8 @@ get_header();
         </div>
   </div>
 </section>
+
+
 <?php get_template_part( 'template-parts/join-us'); ?>
 </main>
 
