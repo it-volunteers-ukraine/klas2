a9cfc5a945123730d29f8f9cdb443660f73d0ce9
Yevhenii2022 <3starosta3@gmail.com>
2023-11-23 11:16:50 +0200

    add projects page structure

---
+++ templates/projects.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Template Name: projects
+*/
+get_header();
+?>
+
+<main>
+  <h2>Наші проєкти</h2>
+
+  <?php get_template_part('template-parts/join-us'); ?>
+</main>
+
+<?php get_footer(); ?>
