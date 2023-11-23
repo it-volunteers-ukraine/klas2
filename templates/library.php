f815cf47be7df93faf4f7309c067d517629e5343
Olga <112238837+OlgaSmile@users.noreply.github.com>
2023-11-23 23:30:58 +0200

    finished gallery html & css on the library page

--- templates/library.php
+++ templates/library.php
@@ -19,20 +19,41 @@ get_header();
       $loop = new WP_Query($args);
       while ( $loop->have_posts() ) {
         $loop->the_post();
+        $iconType = get_field('svg_media_button');
+        $buttonText = get_field('text_media_button');
         ?>
         <div class="entry-content">
           <div class="feature-image">
-              <a class="feature-image__link" href="<?php the_permalink(); ?>" aria-label="Читати детальніше про <?php the_title(); ?>"><?php the_post_thumbnail(); ?></a>
+              
+              <?php if($iconType==="play"): ?>
+                <a target="_blank"  rel="noopener noreferrer" class="feature-image__link" href="<?php the_field('video_media'); ?>" aria-label="Подивитись <?php the_title(); ?>"><?php the_post_thumbnail(); ?></a>
+                <a target="_blank" rel="noopener noreferrer" href="<?php the_field('video_media'); ?>" class="play-outline__button" aria-label="Подивитись <?php the_title(); ?>">
+                  <svg class="play-outline__image" width="32px" height="32px">
+                    <use href="<?php echo get_template_directory_uri()?>/assets/images/sprite.svg#play-outline"></use>
+                  </svg>
+                </a>
+              <?php endif; ?>
+              <?php if($iconType==="download"): ?>
+                <a download class="feature-image__link" href="<?php the_field('pdf_media'); ?>" aria-label="Завантажити <?php the_title(); ?>"><?php the_post_thumbnail(); ?></a>
+              <?php endif; ?>
           </div>
          <div class="library-post__title"> <?php the_title(); ?></div>
-          <a href="" class="button download_button" type="button"><svg class="download-icon" width="24px" height="24px">
-        <use href="<?php echo get_template_directory_uri()?>/assets/images/sprite.svg#download"></use>
-    </svg>Завантажити
-  </a>
-  <!-- <?php the_content(); ?> -->
+         <?php if($iconType==="play"): ?>
+                <a target="_blank" rel="noopener noreferrer" href="<?php the_field('video_media'); ?>" class="button download_button">
+                  <svg class="download-icon" width="24px" height="24px">
+                    <use href="<?php echo get_template_directory_uri()?>/assets/images/sprite.svg#<?php echo $iconType; ?>"></use>
+                  </svg><?php echo $buttonText; ?>
+                </a>
+              <?php endif; ?>
+              <?php if($iconType==="download"): ?>
+                <a download href="<?php the_field('pdf_media'); ?>" class="button download_button">
+                  <svg class="download-icon" width="24px" height="24px">
+                    <use href="<?php echo get_template_directory_uri()?>/assets/images/sprite.svg#<?php echo $iconType; ?>"></use>
+                  </svg><?php echo $buttonText; ?>
+                </a>
+              <?php endif; ?>
         </div>
-        <?php
-      } ?>
+        <?php } ?>
     </div>
     </div>
 </section>
