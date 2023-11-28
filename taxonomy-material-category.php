2f7ce33f95154e40c35248307c196b4c1b1f5a14
Olga <112238837+OlgaSmile@users.noreply.github.com>
2023-11-29 01:16:44 +0200

    added pagination on the library page & taxonomy page

--- taxonomy-material-category.php
+++ taxonomy-material-category.php
@@ -6,22 +6,44 @@ get_header();
 <main>
 <section class="library-section">
     <div class="container">
-      <h1 class="library__title"><?php the_field('library_title', 16) ?></h1>
+      <h1 class="library__title"><?php the_field('library_title', 55) ?></h1>
 
       <?php get_template_part( 'template-parts/material-categories'); ?>
 
-      <div class="library-wrap">
-        <?php if (have_posts()) {
-        while (have_posts()) {
-     the_post();    
-          
-            get_template_part( 'template-parts/content-search' );
+      <?php global $query_string;
+        query_posts($query_string.'&post_type=materials&posts_per_page=3');
+         if (have_posts()) { ?>
+          <div class="library-wrap">
+            <?php while (have_posts()) {
+              the_post();    
+              
+                get_template_part( 'template-parts/content-search' );
+            } ?>
+        </div>
+      <?php 
+      $posts_per_page = 3;
+      $total_posts = wp_count_posts('materials')->publish;
+      $total_pages = ceil($termCount / $posts_per_page);
 
-        }
-      }
-       wp_reset_postdata(); ?>
+      $current_page = get_query_var('paged') ? get_query_var('paged') : 1;
 
-    </div>
+      $args = array(
+        'base' => get_pagenum_link(1) . '%_%',
+        'format' => 'page/%#%',
+        'current' => $current_page,
+        'total' => $total_pages,
+        'prev_text'          => __('<'),
+        'next_text'          => __('>'),
+        'class'              => 'material-pagination', 
+      );
+      echo '<div class="events__pagination">';
+      echo paginate_links($args);
+      echo '</div>';
+
+    }
+    
+        ?>
+
     </div>
 </section>
 </main>
--- template-parts/material-categories.php
+++ template-parts/material-categories.php
@@ -11,14 +11,17 @@
 
         if($cats) : ?>
         <ul class='library-cats__list'>
-          <?php $class = ( $post->ID == 16 ) ? 'active' : ''; ?>
+          <?php $class = ( $post->ID == 55 ) ? 'active' : ''; ?>
            <li class="library-cats__item <?php echo $class; ?>" >
-              <a href="<?php echo get_permalink(16); ?>">
+              <a href="<?php echo get_permalink(55); ?>">
                       Всі матеріали
                   </a>
             </li>
           <?php foreach($cats as $cat) {
             $curTerm = $wp_query->queried_object;
+            global $termCount;
+            $termCount = $curTerm->count;
+
             $class = ( $cat->name == $curTerm->name ) ? 'active' : '';
             $termId = $cat->term_id;
             ?>
