b6e1a40360038885c9623529f02718c33ee0e65e
Olga <112238837+OlgaSmile@users.noreply.github.com>
2023-11-29 01:17:02 +0200

    Create pagination.php

---
+++ template-parts/pagination.php
@@ -0,0 +1,6 @@
+<div class="navigation">
+  
+    <div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'creative' ) ); ?></div>
+    <div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'creative' ) ); ?></div>
+
+</div>
