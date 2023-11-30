72c494b0d996a44752d6f65be1e013eb00b6247b
Olga <112238837+OlgaSmile@users.noreply.github.com>
2023-11-30 20:43:12 +0200

    added donate section on support page

--- templates/support.php
+++ templates/support.php
@@ -10,19 +10,25 @@ get_header();
       <h1 class="support-title"><?php the_title(); ?></h1>
     </div>
   </section>
-<section class="support-section">
+<section class="donate-section">
   <div class="container">
-    <div class="support__inner-container">
-      <div class="support-text__wrap">
-        <h2 class="support-section__title"><?php the_field('donate_title'); ?></h2>
-        <p class="support-section__subtitle"><?php the_field('donate_subtitle'); ?><a href="" class="support-section__bank"><?php the_field('donate_bank'); ?></a></p>
+    <div class="donate__inner-container">
+      <div class="donate-text__wrap">
+        <h2 class="donate-section__title"><?php the_field('donate_title'); ?></h2>
+        <p class="donate-section__subtitle"><?php the_field('donate_subtitle'); ?><a href="" class="donate-section__bank"><?php the_field('donate_bank'); ?></a></p>
       </div>
-      <div class="support-image__wrap">
-        <img class="support-img" width="250" height="250" src='<?php the_field('donate_qr-code'); ?>' alt='qr-code'>
+      <a class="button primary_button donate-button" href=""><?php the_field('donate_button'); ?></a>
+      <div class="donate-image__wrap">
+        <img class="donate-img" width="250" height="250" src='<?php the_field('donate_qr-code'); ?>' alt='qr-code'>
       </div>
-      <a class="button primary_button" href=""><?php the_field('donate_button'); ?></a>
     </div>
   </div>
+
+  <section class="middle-section">
+    <div class="container">
+      
+    </div>
+  </section>
   
 </section>
 </main>
