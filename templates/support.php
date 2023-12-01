1322e38dd1c007b95f4a3af6be7570aa882bf8fe
Olga <112238837+OlgaSmile@users.noreply.github.com>
2023-12-01 12:53:38 +0200

    Update support.php

--- templates/support.php
+++ templates/support.php
@@ -16,9 +16,9 @@ get_header();
       <div class="donate__inner-container">
         <div class="donate-text__wrap">
           <h2 class="donate-section__title"><?php the_field('donate_title'); ?></h2>
-          <p class="donate-section__subtitle"><?php the_field('donate_subtitle'); ?><a href="" class="donate-section__bank"><?php the_field('donate_bank'); ?></a></p>
+          <p class="donate-section__subtitle"><?php the_field('donate_subtitle'); ?><a href="<?php the_field('bank_link'); ?>" class="donate-section__bank"><?php the_field('donate_bank'); ?></a></p>
         </div>
-        <a class="button primary_button donate-button" href=""><?php the_field('donate_button'); ?></a>
+        <a class="button primary_button donate-button" href="<?php the_field('bank_link'); ?>"><?php the_field('donate_button'); ?></a>
         <div class="donate-image__wrap">
           <img class="donate-img" width="250" height="250" src='<?php the_field('donate_qr-code'); ?>' alt='qr-code'>
         </div>
@@ -31,7 +31,7 @@ get_header();
       <div class="support_inner__container">
         <div class="support-image__wrap">
           <div class="support-image_wrapper">
-            <img class="support-img" src='<?php the_field('support_middle_image'); ?>' alt=''>
+            <img class="support-img" src='<?php the_field('support_middle_image'); ?>' alt='<?php the_field('support_image_alt') ?>'>
           </div>
           <svg class="support-left__image" width="112px" height="98px">
             <use href="<?php echo get_template_directory_uri()?>/assets/images/sprite.svg#fon_left"></use>
