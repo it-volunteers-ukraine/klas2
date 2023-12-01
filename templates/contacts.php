fdcef94fe9021d97ba1439a10b24e7bb9fe1e88d
Diana Forostiana <dianaforost@gmail.com>
2023-12-01 10:13:28 +0100

    fix: icons

--- templates/contacts.php
+++ templates/contacts.php
@@ -12,14 +12,7 @@ get_header();
     <div class="contacts__wrapper">
       <ul class="contacts__list">
               <li class="contacts__list__item">
-              <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect x="0.5" y="0.5" width="39" height="39" rx="19.5" stroke="#ACACAC"/>
-<path d="M14 27.9208V19.721L11.3639 17.3945L9 16.1035V26.4738C9 27.2744 9.67246 27.9208 10.5 27.9208H14Z" fill="#4285F4"/>
-<path d="M26 27.9207H29.5C30.33 27.9207 31 27.2719 31 26.4737V16.1035L28.3225 17.5822L26 19.7209V27.9207Z" fill="#34A853"/>
-<path d="M13.9993 19.7215L13.6406 16.5176L13.9993 13.4512L19.9993 17.7922L25.9993 13.4512L26.4005 16.3521L25.9993 19.7215L19.9993 24.0626L13.9993 19.7215Z" fill="#EA4335"/>
-<path d="M26 13.4511V19.7214L31 16.1039V14.1746C31 12.3851 28.8825 11.365 27.4001 12.4382L26 13.4511Z" fill="#FBBC04"/>
-<path d="M9 16.104L11.2996 17.7678L14 19.7215V13.4511L12.5999 12.4382C11.1149 11.365 9 12.3852 9 14.1745V16.1039V16.104Z" fill="#C5221F"/>
-</svg>
+              <svg class="contacts__list__item__icon"><use href="<?php echo get_template_directory_uri()?>/assets/images/sprite.svg#gmail"></use></svg>
                   <p class="contacts__list__item__text"><?php the_field('email') ?></p>
               </li>
               <li class="contacts__list__item">
