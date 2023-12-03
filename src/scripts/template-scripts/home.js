e6bde6a6caab7b5ded5096489f22c603c4a621e6
Tetiana Fedorenko <1031213@gmail.com>
2023-12-03 12:30:42 +0200

    add slider

--- src/scripts/template-scripts/home.js
+++ src/scripts/template-scripts/home.js
@@ -1,1 +1,23 @@
-console.log('home page');
+$(document).ready(function(){
+    console.log("home page");	    console.log("home page");
+
+   $('.slider__inner').slick({
+    slidesToShow:3,
+    slidesToScroll: 1,
+    centerMode: true,
+    speed:2000,
+    arrows: false,
+    dots:false,
+    fade: false,
+    //easing:'ease-in',
+    Infinite: true,
+    autoplay:true,
+    autoplaySpeed:2000,
+    pauseOnFocus:false,
+    pauseOnHover:false,
+    pauseOnDotsHover:false,
+    variableWidth:true,
+
+   });
+
+});
