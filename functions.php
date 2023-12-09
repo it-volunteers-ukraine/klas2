990f61803c980f2ae269c23d4cab1242f8e13d85
Olga <112238837+OlgaSmile@users.noreply.github.com>
2023-12-09 12:24:52 +0200

    change the order of scripts in the functions.php

--- functions.php
+++ functions.php
@@ -25,9 +25,9 @@ add_action('wp_enqueue_scripts', 'wp_it_volunteers_scripts');
 function wp_it_volunteers_scripts()
 {
   wp_enqueue_style('main', get_stylesheet_uri());
-  wp_enqueue_style('swiper-style', "https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css", array());
-  wp_enqueue_style('wp-it-volunteers-style', get_template_directory_uri() . '/assets/styles/main.css', array('swiper-style', 'main'));
-  wp_enqueue_style('normalize', 'https://cdnjs.cloudflare.com/ajax/libs/modern-normalize/2.0.0/modern-normalize.min.css');
+  wp_enqueue_style('wp-it-volunteers-style', get_template_directory_uri() . '/assets/styles/main.css', array('main'));
+  wp_enqueue_style('swiper-style', "https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css", array('main'));
+  wp_enqueue_style('normalize', 'https://cdnjs.cloudflare.com/ajax/libs/modern-normalize/2.0.0/modern-normalize.min.css', array('main'));
 
   wp_enqueue_script('swiper-scripts', 'https://cdn.jsdelivr.net/npm/swiper@10.0.0/swiper-bundle.min.js', array(), false, true);
   wp_enqueue_script('wp-it-volunteers-scripts', get_template_directory_uri() . '/assets/scripts/main.js', array('swiper-scripts'), false, true);
