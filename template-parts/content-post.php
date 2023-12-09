d097c5534d88569d8944d26af095ec2622ccd5a3
litvinenkoiryna <litvinenkoiryna@gmail.com>
2023-12-09 15:39:22 +0200

    border-radius 5px,banner-title max-width in %

--- template-parts/content-post.php
+++ template-parts/content-post.php
@@ -5,12 +5,12 @@
                 <h1 class="single__post-title"><?php the_title(); ?></h1>
                 <p class="single__post-time"><?php the_time('d.m.y'); ?></p>
             </div>
-            <div class="main__post">
+            <div class="single__container">
                 <div class="post__content">
                     <?php the_content(); ?>
                     <?php if(have_rows('gallery')):?>
-                        <div class="single-post__slider2">
-                            <ul class="swiper-wrapper" id="lightgallery">
+                        <div class="single-post__slider2 swiper-container">
+                            <ul class="swiper-wrapper " id="lightgallery">
                                 <?php while(have_rows('gallery')): the_row();
                                      $img = get_sub_field('post-img');  ?>
                                         <li class="swiper-slide">
@@ -20,24 +20,29 @@
                                 <?php endwhile;?>
                                              
                             </ul>
-                               <div class="swiper-button-next"></div>
-                                <div class="swiper-button-prev"></div>         
+                            <div class="swiper-pagination"></div>  
+                            <div class="swiper-button-next"></div>
+                            <div class="swiper-button-prev"></div>        
                         </div>
+                        
                         <div thumbsSlider="" class="single-post__slider">
-                            <ul class="swiper-wrapper">
+                            <ul class="swiper-wrapper ">
                                 <?php while(have_rows('gallery')): the_row();
                                     $img = get_sub_field('post-img');  ?>
                                         <li class="swiper-slide">
                                             <img id="image-link" src="<?php echo esc_url($img); ?>" alt="img">
                                         </li>
+                                       
                                 <?php endwhile;?>
                             </ul>
-                    <?php endif; ?>
+                    <?php endif; ?> 
+                             
                         </div>
-                </div>
-                
-                </div>
-            </div>
+
+               </div>
+               <?php get_template_part( 'template-parts/latest-posts'); ?>
+            </div> 
+            
       </div>
 </section>
 
