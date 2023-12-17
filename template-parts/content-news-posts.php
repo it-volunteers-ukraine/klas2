979af310ce789e790f19cfcbb617e67659b94e4b
litvinenkoiryna <litvinenkoiryna@gmail.com>
2023-12-18 01:43:33 +0200

    template-part content-banner

--- template-parts/content-news-posts.php
+++ template-parts/content-news-posts.php
@@ -9,22 +9,7 @@ get_header();
         <h1 class="main__title">Новини</h1>
         <!-- banner  -->
         <div class="news-main__banner">
-            <?php 
-            $latest_post = get_posts('numberposts=1');
-                if($latest_post) {
-                    foreach($latest_post as $post) :
-                        setup_postdata($post);
-            ?>                
-                <img alt="main banner" src="<?php the_field('main-banner'); ?>">
-                <h2 class="news-banner__title"><?php the_title(); ?></h2>
-                <a href="<?php the_permalink(); ?>" class="button news-banner__button">Читати більше<svg class="banner__arrow-icon">
-                            <use href="<?php echo get_template_directory_uri()?>/assets/images/sprite.svg#arrow"></use>
-                            </svg></a>
-            <?php
-                endforeach;
-            }
-            ?>
-        <?php wp_reset_postdata(); ?>
+            <?php get_template_part('template-parts/content-banner'); ?>
         </div>
 
             <!-- posts  -->
